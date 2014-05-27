0da70afa8aafaf81698a6dd2b3ea86de50edb228
Rusty Holleman <rustychris@gmail.com>
2014-05-27 18:47:22 +0400

    tweaks to work on wakari

--- python-cgal-bindings/build.sh
+++ python-cgal-bindings/build.sh
@@ -4,6 +4,7 @@
 SWIG_VER=`swig -swiglib | sed -e 's|^.*/share/swig/||'`
 export SWIG_LIB=$PREFIX/share/swig/${SWIG_VER}
 cmake -DBUILD_JAVA=OFF -DSWIG_DIR=$SYS_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX .
-make -j
+# on wakari, don't use -j b/c of RAM constraints
+make -j2
 cp -av build-python/CGAL $SP_DIR/CGAL
 cp -av lib/* $PREFIX/lib
