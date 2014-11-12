bce8a82a8cc4cb30ac283f206baff9aaab61e92a
Rusty Holleman <rustychris@gmail.com>
2014-11-12 08:14:20 -0500

    CGAL: Fix cmake invocation to pass in -Lpath -lgmp.  Compiles on OSX now.

--- cgal/build.sh
+++ cgal/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cmake -DGMP_LIBRARIES=$SYS_PREFIX/lib -DGMP_INCLUDE_DIR=$SYS_PREFIX/include -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=$ORIGIN/../lib' .
-make
+# setting GMP_LIBRARIES with the full -L<path> -lgmp
+# was necessary on OSX/clang.  Not sure if it somehow worked on linux
+# with just the path
+cmake -DGMP_LIBRARIES="-L$SYS_PREFIX/lib -lgmp" -DGMP_INCLUDE_DIR=$SYS_PREFIX/include -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=$ORIGIN/../lib' .
+make 
 make install
