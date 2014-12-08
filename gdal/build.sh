feee5bcc33afc15234030b441e8b61c5d737b750
Rusty Holleman <rustychris@gmail.com>
2014-12-08 08:49:59 -0500

    Detect OSX by either spelling of OSX_ARCH

--- gdal/build.sh
+++ gdal/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # trying this for OSX, because rpath is not long enough sometimes.
-if [ -n "$OSXARCH" ] ; then
+# but now it's not working...
+# seems that the naming might have changed
+if [ -n "${OSX_ARCH}${OSXARCH}" ] ; then
  export LDFLAGS=-headerpad_max_install_names 
 fi
 
 # explicitly tell it to use bash, otherwise there are libtool problems.
-CONFIG_SHELL=/bin/bash bash configure --with-python --prefix=$PREFIX
-make -j
+# trying the static proj.4
+CONFIG_SHELL=/bin/bash bash configure --with-python --prefix=$PREFIX --with-static-proj4
+make 
 make install
 
 # not sure why this happens - but it's problematic
